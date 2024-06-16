58ac72d19f9194318a32275de53e81a94d464d58
Yonatan Mizrahi <yonatanmiz963@gmail.com>
2024-06-16 14:56:30 +0300

    getting dbURL from env variables

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://Yonatan:fyMlDFC02opvqwwh@atlascluster.uffzyax.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster',
-  // 'dbURL': 'mongodb+srv://Yonatan:1234@cluster0.ogahi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  'dbURL': process.env.dbURL,
 }
